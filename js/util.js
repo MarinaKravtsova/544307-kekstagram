38e78db9184c2687af533f0e744087d63041b181
Marina <kanariekanarie@rambler.ru>
2018-02-23 17:10:17 +0500

    исправила

--- js/util.js
+++ js/util.js
@@ -1,19 +1,60 @@
 'use strict';
 
-window.util = (function () {
+(function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
+  var DEBOUNCE_INTERVAL = 500; // ms
 
-  return {
-    isEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
-        action();
-      }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action();
-      }
+  /**
+  * Функция объявляюшее событие при нажатии на кнопку Esc
+  * @param  {type} evt    событие
+  * @param  {type} action
+  */
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
     }
+  };
+
+  /**
+  * Функция объявляюшее событие при нажатии на кнопку Enter
+  * @param  {type} evt    событие
+  * @param  {type} action
+  */
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  };
+
+  /**
+  * Функция генерирующая случайное число от max до min
+  * @param {number} max Максимальное число
+  * @param {number} min Минимальное число
+  * @return {number}
+  */
+  var getRandomNumber = function (max, min) {
+    var RandomNumber = Math.round(Math.random() * (max - min) + min);
+
+    return RandomNumber;
+  };
+
+  /**
+   * Функция устраняющая дребезг
+   * @param  {type} fun
+   */
+  var debounce = function (fun) {
+    var lastTimeout;
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
+  };
+
+  window.util = {
+    isEscEvent: isEscEvent,
+    isEnterEvent: isEnterEvent,
+    getRandomNumber: getRandomNumber,
+    debounce: debounce
   };
 })();
